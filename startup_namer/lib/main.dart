0e9b2f8bd12c63cf3a360b7f81cb3f2744312f0b
akm <akm2000@gmail.com>
2022-01-23 16:21:01 +0900

    :sparkles: Add _suggestions and _biggerFont to _RandomWordsState

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -35,6 +35,9 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
