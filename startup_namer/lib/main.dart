4bedb4d145de1e644a4a2fd02a1e9334da65aaf7
akm <akm2000@gmail.com>
2022-01-23 16:21:36 +0900

    :sparkles: Add a _buildSuggestions() function to the _RandomWordsState class

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -43,4 +43,18 @@ class _RandomWordsState extends State<RandomWords> {
     final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 }
